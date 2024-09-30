6899cad0ba2f9fbf9af0be55dbd166f82bea7f68
Patrick Gray <patrickgray1996@gmail.com>
2024-09-30 20:36:11 +1300

    complete contact page

--- src/pages/Contact.jsx
+++ src/pages/Contact.jsx
@@ -3,6 +3,8 @@ import emailjs from 'emailjs-com';
 import { useMediaQuery } from 'react-responsive';
 import PetContact from '../Images/GuideImages/Pet-on-ridge.jpg';
 import PetFootstoolSkiDescent from '../Images/GuideImages/PetFootstoolSkiDescent.jpg';
+import { FaWhatsapp } from 'react-icons/fa';
+
 export default function Contact() {
   const [formData, setFormData] = useState({
     name: '',
@@ -46,7 +48,7 @@ export default function Contact() {
   };
 
   return (
-    <div className="flex flex-col md:flex-row">
+    <div className="flex flex-col md:flex-row bg-blue-100 text-dark-blue">
       {isMediumOrLarger ? (
         <img
           src={PetFootstoolSkiDescent}
@@ -56,58 +58,76 @@ export default function Contact() {
       ) : (
         <img src={PetContact} alt="Petrouchka on Ridge in Norway" className="w-full h-auto shadow-lg" />
       )}
-      <form className="bg-white p-6 rounded-lg shadow-md w-full md:w-1/2" onSubmit={handleSubmit}>
-        <input
-          type="text"
-          id="name"
-          name="name"
-          required
-          placeholder="Name"
-          aria-label="Name"
-          className="w-full p-2 border border-gray-300 rounded-md mb-4"
-          value={formData.name}
-          onChange={handleChange}
-        />
+      <div className="w-full md:w-1/2 flex flex-col items-center justify-evenly">
+        <h2 className="text-center text-2xl font-semibold mb-4 mt-5">
+          <i>Get in touch today if you have a dream climb and ski that you want to make a reality</i>
+        </h2>
+        <a
+          href="https://wa.me/0225618309?text=Hello!%20I%20would%20like%20to%20inquire%20about%20your%20services."
+          target="_blank"
+          rel="noopener noreferrer"
+          className="inline-block bg-green-500 text-white p-3 rounded-md mb-4 hover:bg-green-600 m-5"
+        >
+          Contact us on WhatsApp <FaWhatsapp className="inline text-lg" />
+        </a>
+        <p className="text-xl pb-6 text-center leading-loose">
+          OR
+          <br />
+          Please Complete this form:
+        </p>
+        <form className="bg-white p-6 rounded-lg shadow-md w-full text-black" onSubmit={handleSubmit}>
+          <input
+            type="text"
+            id="name"
+            name="name"
+            required
+            placeholder="Name"
+            aria-label="Name"
+            className="w-full p-2 border border-gray-300 rounded-md mb-4"
+            value={formData.name}
+            onChange={handleChange}
+          />
 
-        <input
-          type="tel"
-          id="phone"
-          name="phone"
-          required
-          placeholder="Phone"
-          aria-label="Phone"
-          className="w-full p-2 border border-gray-300 rounded-md mb-4"
-          value={formData.phone}
-          onChange={handleChange}
-        />
+          <input
+            type="tel"
+            id="phone"
+            name="phone"
+            required
+            placeholder="Phone"
+            aria-label="Phone"
+            className="w-full p-2 border border-gray-300 rounded-md mb-4"
+            value={formData.phone}
+            onChange={handleChange}
+          />
 
-        <input
-          type="email"
-          id="email"
-          name="email"
-          required
-          placeholder="Email"
-          aria-label="Email"
-          className="w-full p-2 border border-gray-300 rounded-md mb-4"
-          value={formData.email}
-          onChange={handleChange}
-        />
+          <input
+            type="email"
+            id="email"
+            name="email"
+            required
+            placeholder="Email"
+            aria-label="Email"
+            className="w-full p-2 border border-gray-300 rounded-md mb-4"
+            value={formData.email}
+            onChange={handleChange}
+          />
 
-        <textarea
-          id="message"
-          name="message"
-          required
-          placeholder="Message"
-          aria-label="Message"
-          className="w-full p-2 border border-gray-300 rounded-md mb-4"
-          value={formData.message}
-          onChange={handleChange}
-        ></textarea>
+          <textarea
+            id="message"
+            name="message"
+            required
+            placeholder="Message"
+            aria-label="Message"
+            className="w-full p-2 border border-gray-300 rounded-md mb-4"
+            value={formData.message}
+            onChange={handleChange}
+          ></textarea>
 
-        <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
-          Send
-        </button>
-      </form>
+          <button type="submit" className="w-full bg-blue-500 text-white p-2 rounded-md hover:bg-blue-600">
+            Send
+          </button>
+        </form>
+      </div>
     </div>
   );
 }
